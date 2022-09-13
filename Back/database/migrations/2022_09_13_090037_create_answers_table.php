2709be7696fd79766bfce764d66869e7e0f46e37
Adama <adamagassama40.ag@gmail.com>
2022-09-13 11:26:58 +0200

    Creation table answers

---
+++ Back/database/migrations/2022_09_13_090037_create_answers_table.php
@@ -0,0 +1,44 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('answers', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('answer');
+
+            $table->unsignedInteger('question_id')->nullable();
+            $table->foreign('question_id')->references('id')->on('questions');
+
+            $table->unsignedInteger('visitor_id')->nullable();
+            $table->foreign('visitor_id')->references('id')->on('visitors');
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('answers');
+    }
+};
+
+
+
+
+
