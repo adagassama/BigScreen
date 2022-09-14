9e9a5214f0d6569efd848b78cc796447d73a29d2
Adama <adamagassama40.ag@gmail.com>
2022-09-14 22:18:05 +0200

    Added routes for Api answer & Visitor

--- Back/routes/api.php
+++ Back/routes/api.php
@@ -1,6 +1,8 @@
 <?php
 
+use App\Http\Controllers\Api\AnswerController;
 use App\Http\Controllers\Api\QuestionController;
+use App\Http\Controllers\Api\VisitorController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -20,3 +22,5 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 });
 
 Route::apiResource('questions',QuestionController::class);
+Route::apiResource('answers',AnswerController::class);
+Route::apiResource('visitors',VisitorController::class);
