61dab4f16bee57342b1933a8fc21afcd4b7394ca
CKankou04 <>
2022-09-19 14:32:14 +0200

    form for viewing survey responses

---
+++ Front/src/views/ResponseView.vue
@@ -0,0 +1,14 @@
+<template>
+  <FormResponse></FormResponse>
+</template>
+
+<script>
+import FormResponse from '../components/FormResponse.vue';
+export default {
+    components: { FormResponse }
+}
+</script>
+
+<style>
+
+</style>
