5d2527dfc5bef4feb07f8fb416d04b247b9b5c42
CKankou04 <>
2022-09-19 17:43:39 +0200

    administrator authentication

--- Front/src/views/LoginView.vue
+++ Front/src/views/LoginView.vue
@@ -1,15 +1,12 @@
 <template>
-<Login></Login>
+  <Login></Login>
 </template>
 
 <script>
-import Login from '../components/Login.vue';
+import Login from "../components/Login.vue";
 
 export default {
-    components: { Login }
-}
+  components: { Login },
+};
 </script>
 
-<style>
-
-</style>
