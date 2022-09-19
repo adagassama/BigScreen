51b89b9b98a7dafa1b3861c8abf02f5c74cd32d9
CKankou04 <>
2022-09-19 17:39:10 +0200

    Authentication Controller and login function

---
+++ Back/app/Http/Controllers/AuthController.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use Illuminate\Validation\ValidationException;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+
+class AuthController extends Controller
+{
+    public function login(Request $request)
+    {
+        $request->validate([
+            'email' => 'required|email',
+            'password' => 'required',
+            'device_name' => 'required',
+        ]);
+    
+        $user = User::where('email', $request->email)->first();
+    
+        if (! $user || ! Hash::check($request->password, $user->password)) {
+            throw ValidationException::withMessages([
+                'email' => ['Les informations identification fournies sont incorrectes'],
+            ]);
+        }
+    
+        return $user->createToken($request->device_name)->plainTextToken;
+    }
+
+   /*  public function logout(Request $request)
+    {
+        $request->user()->currentAccessToken()->delete();
+        return response()->json(['msg' => '']);
+    } */
+}
