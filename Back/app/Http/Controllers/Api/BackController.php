e0984b91b4a10b4c1b7d92a7a6f393253cad68ad
Adama <adamagassama40.ag@gmail.com>
2022-09-23 13:03:23 +0200

    Get Pie Charts Data

---
+++ Back/app/Http/Controllers/Api/BackController.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use App\Models\Answer;
+use App\Models\Question;
+
+class BackController extends Controller
+{
+    /** Get pie charts by user counted and answers with id*/
+    protected function getPieChart($id)
+    {
+        $answers = Answer::where('question_id', $id)->get();
+        $possible_answers = Question::where('id', $id)->get();
+        $options = explode(',', $possible_answers[0]->possible_answer);
+        $stats = [];
+
+        foreach ($options as $option) {
+
+            $count = 0;
+            foreach ($answers as $answer) {
+                if ($answer->answer == $option) {
+                    $count = $count += 1;
+                }
+            }
+            array_push($stats, $count);
+
+        }
+        //dd($stats);
+        return $stats;
+    }
+}
