23e378ac2f19e85189186b2f6ea3013c422525cf
CKankou04 <>
2022-09-23 12:55:13 +0200

    changement Sidebar

--- Front/src/components/Dashboard.vue
+++ Front/src/components/Dashboard.vue
@@ -1,50 +1,51 @@
 <template>
-  <div id="wrapper">
-   <div class="overlay"></div>
-    
-        <!-- Sidebar -->
-    <nav class="navbar navbar-inverse fixed-top" id="sidebar-wrapper" role="navigation">
-        <ul class="nav sidebar-nav">
-            <div class="sidebar-header">
-                <div class="sidebar-brand">
-                    <p>BIGSCREEN</p>
-                </div>
-            </div>
-            <li @click="display(1)" ><a href="#">Accueil</a></li>
-            <li @click="display(2)"><a href="#">Questionnaire</a></li>
-            <li @click="display(3)"><a href="#">Réponse</a></li>
-        </ul>
-    </nav>
-        <div id="page-content-wrapper">
-            <button type="button" class="hamburger animated fadeInLeft is-closed" data-toggle="offcanvas">
-                <span class="hamb-top"></span>
-    			<span class="hamb-middle"></span>
-				<span class="hamb-bottom"></span>
-            </button>
-            <div class="container dashboard">
-                <div class="row">
-                    
-                        <h1>Bienvenue sur la Dashboard</h1>
-                        <Accueil v-if="page==1"></Accueil>
-                        <Questionnaire v-else-if="page==2"></Questionnaire>
-                        <Reponse v-else></Reponse>
-                    
-                </div>
-            </div>
-        </div>
-        <!-- /#page-content-wrapper -->
+  <div class=" dashboard">
+        <!-- Sidebar  -->
+        <nav id="sidebar">
+            <ul class="list-unstyled components pt-5 mt-5 text-center">
+                <img src="../assets/bigscreen_logo.png" alt="le logo" style="width:15rem" />
+                <li class="text-center"  @click="display(1)">
+                    <a href="#" >Accueil</a>
+                </li>
+                <li class="text-center"  @click="display(2)">
+                    <a href="#">Questionnaires</a>
+                </li>
+                <li class="text-center"  @click="display(3)">
+                    <a href="#" >Réponses</a>
+                </li> 
+            </ul>
+            <ul class="list-unstyled CTAs">
+                <li>
+                    <a href="#" class="download">Déconnexion</a>
+                </li>
+            </ul>
+        </nav>
 
+        <!-- Page Content  -->
+        <div id="content">
+            <nav class="navbar navbar-expand-lg navbar-light">
+                <div class="container-fluid">
+                    <button type="button" id="sidebarCollapse" class="btn btn-info">
+                        <i class="fas fa-align-left"></i>
+                        <span>Toggle</span>
+                    </button>
+                </div>
+            </nav>
+              <Accueil v-if="page==1"></Accueil>
+              <Questionnaire v-else-if="page==2"></Questionnaire>
+              <Reponse v-else></Reponse> 
+        </div>
     </div>
-    <!-- /#wrapper -->
 </template>
 
 <script>
-    import $ from 'jquery'
+import $ from 'jquery'
 import Accueil from './Accueil.vue';
 import Questionnaire from './Questionnaire.vue';
 import Reponse from './Reponse.vue';
 import axios from "axios";
 window.axios = axios;
+
 export default {
     data(){
         return{
@@ -56,329 +57,150 @@ export default {
             this.page = k;
         }
     },
-    mounted() {
+    components:{
+        Accueil, Questionnaire, Reponse
+    },
+    mounted(){
         window.axios.defaults.headers.common['Authorization'] = `Bearer ${this.token}`
         $(document).ready(function () {
-            var trigger = $(".hamburger"), overlay = $(".overlay"), isClosed = false;
-            trigger.click(function () {
-                hamburger_cross();
+                $('#sidebarCollapse').on('click', function () {
+                    $('#sidebar').toggleClass('active');
+                });
             });
-            function hamburger_cross() {
-                if (isClosed == true) {
-                    overlay.hide();
-                    trigger.removeClass("is-open");
-                    trigger.addClass("is-closed");
-                    isClosed = false;
-                }
-                else {
-                    overlay.show();
-                    trigger.removeClass("is-closed");
-                    trigger.addClass("is-open");
-                    isClosed = true;
-                }
-            }
-            $("[data-toggle=\"offcanvas\"]").click(function () {
-                $("#wrapper").toggleClass("toggled");
-            });
-        });
-    },
-    components: { Accueil, Questionnaire, Reponse }
+    }
 }
 </script>
 
 <style>
 
-
-
-.nav .open > p, 
-.nav .open > p:hover, 
-.nav .open > p:focus {background-color: transparent;}
-
-#wrapper {
-    padding-left: 0;
-    -webkit-transition: all 0.5s ease;
-    -moz-transition: all 0.5s ease;
-    -o-transition: all 0.5s ease;
-    transition: all 0.5s ease;
+p {
+    font-family: 'Poppins', sans-serif;
+    font-size: 1.1em;
+    font-weight: 300;
+    line-height: 1.7em;
+    color: #999;
 }
 
-#wrapper.toggled {
-    padding-left: 220px;
+a,
+a:hover,
+a:focus {
+    color: inherit;
+    text-decoration: none;
+    transition: all 0.3s;
 }
 
-#sidebar-wrapper {
-    z-index: 1000;
-    left: 220px;
-    width: 0;
-    height: 100%;
-    margin-left: -220px;
-    overflow-y: auto;
-    overflow-x: hidden;
-    background: #1a1a1a;
-    -webkit-transition: all 0.5s ease;
-    -moz-transition: all 0.5s ease;
-    -o-transition: all 0.5s ease;
-    transition: all 0.5s ease;
-}
-
-#sidebar-wrapper::-webkit-scrollbar {
-  display: none;
-}
-
-#wrapper.toggled #sidebar-wrapper {
-    width: 220px;
-}
-
-#page-content-wrapper {
-    width: 100%;
-    padding-top: 70px;
-}
-
-#wrapper.toggled #page-content-wrapper {
-    position: absolute;
-    margin-right: -220px;
-}
-
-/*-------------------------------*/
-/*     Sidebar nav styles        */
-/*-------------------------------*/
 .navbar {
-  padding: 0;
-}
-
-.sidebar-nav {
-    position: absolute;
-    top: 0;
-    width: 220px;
-    margin: 0;
-    padding: 0;
-    list-style: none;
-}
-
-.sidebar-nav li {
-    position: relative; 
-    line-height: 20px;
-    display: inline-block;
-    width: 100%;
-}
-
-.sidebar-nav li:before {
-    content: '';
-    position: absolute;
-    top: 0;
-    left: 0;
-    z-index: -1;
-    height: 100%;
-    width: 3px;
-    background-color: #1c1c1c;
-    -webkit-transition: width .2s ease-in;
-      -moz-transition:  width .2s ease-in;
-       -ms-transition:  width .2s ease-in;
-            transition: width .2s ease-in;
-
-}
-.sidebar-nav li:hover{
-  background: skyblue !important;
-  border-radius: 10px;
-  margin-left: 10px;
-  margin-right: 10px;
-}
-.sidebar-nav li:hover:before,
-.sidebar-nav li.open:hover:before {
-    width: 100%;
-    -webkit-transition: width .2s ease-in;
-      -moz-transition:  width .2s ease-in;
-       -ms-transition:  width .2s ease-in;
-            transition: width .2s ease-in;
-
-}
-
-.sidebar-nav li a {
-    display: block;
-    color: #ddd;
-    text-decoration: none;
-    padding: 10px 15px 10px 30px;    
-}
-
-.sidebar-nav li a:hover,
-.sidebar-nav li a:active,
-.sidebar-nav li a:focus,
-.sidebar-nav li.open a:hover,
-.sidebar-nav li.open a:active,
-.sidebar-nav li.open a:focus{
-    color: #fff;
-    text-decoration: none;
-    background-color: transparent;
-}
-.sidebar-header {
-    text-align: center;
-    font-size: 20px;
-    position: relative;
-    width: 100%;
-    display: inline-block;
-}
-.sidebar-brand {
-    height: 65px;
-    position: relative;
-    background:#212531;
-    background: linear-gradient(to right bottom, #2f3441 50%, #212531 50%);
-   padding-top: 1em;
-}
-.sidebar-brand p {
-    color: #ddd;
-}
-.sidebar-brand a:hover {
-    color: #fff;
-    text-decoration: none;
-}
-.dropdown-header {
-    text-align: center;
-    font-size: 1em;
-    color: #ddd;
-    background:#212531;
-    background: linear-gradient(to right bottom, #2f3441 50%, #212531 50%);
-}
-.sidebar-nav .dropdown-menu {
-    position: relative;
-    width: 100%;
-    padding: 0;
-    margin: 0;
-    border-radius: 0;
     border: none;
-    background-color: #222;
+    border-radius: 0;
+    margin-bottom: 40px;
+    box-shadow: 1px 1px 3px rgba(0, 0, 0, 0.1);
+}
+
+.navbar-btn {
     box-shadow: none;
-}
-.dropdown-menu.show {
-    top: 0;
-}
-/*Fontawesome icons*/
-.nav.sidebar-nav li a::before {
-    font-family: fontawesome;
-    content: "\f12e";
-    vertical-align: baseline;
-    display: inline-block;
-    padding-right: 5px;
-}
-a[href*="#"]::before {
-  content: "\f015" ;
-}
-/*-------------------------------*/
-/*       Hamburger-Cross         */
-/*-------------------------------*/
-
-.hamburger {
-  position: fixed;
-  top: 20px;  
-  z-index: 999;
-  display: block;
-  width: 32px;
-  height: 32px;
-  margin-left: 15px;
-  background: transparent;
-  border: none;
-}
-.hamburger:hover,
-.hamburger:focus,
-.hamburger:active {
-  outline: none;
-}
-.hamburger.is-closed:before {
-  content: '';
-  display: block;
-  width: 100px;
-  font-size: 14px;
-  color: #fff;
-  line-height: 32px;
-  text-align: center;
-  opacity: 0;
-  -webkit-transform: translate3d(0,0,0);
-  -webkit-transition: all .35s ease-in-out;
-}
-.hamburger.is-closed:hover:before {
-  opacity: 1;
-  display: block;
-  -webkit-transform: translate3d(-100px,0,0);
-  -webkit-transition: all .35s ease-in-out;
+    outline: none !important;
+    border: none;
 }
 
-.hamburger.is-closed .hamb-top,
-.hamburger.is-closed .hamb-middle,
-.hamburger.is-closed .hamb-bottom,
-.hamburger.is-open .hamb-top,
-.hamburger.is-open .hamb-middle,
-.hamburger.is-open .hamb-bottom {
-  position: absolute;
-  left: 0;
-  height: 4px;
-  width: 100%;
-}
-.hamburger.is-closed .hamb-top,
-.hamburger.is-closed .hamb-middle,
-.hamburger.is-closed .hamb-bottom {
-  background-color: #1a1a1a;
-}
-.hamburger.is-closed .hamb-top { 
-  top: 5px; 
-  -webkit-transition: all .35s ease-in-out;
-}
-.hamburger.is-closed .hamb-middle {
-  top: 50%;
-  margin-top: -2px;
-}
-.hamburger.is-closed .hamb-bottom {
-  bottom: 5px;  
-  -webkit-transition: all .35s ease-in-out;
+.line {
+    width: 100%;
+    height: 1px;
+    border-bottom: 1px dashed #ddd;
+    margin: 40px 0;
 }
 
-.hamburger.is-closed:hover .hamb-top {
-  top: 0;
-  -webkit-transition: all .35s ease-in-out;
-}
-.hamburger.is-closed:hover .hamb-bottom {
-  bottom: 0;
-  -webkit-transition: all .35s ease-in-out;
-}
-.hamburger.is-open .hamb-top,
-.hamburger.is-open .hamb-middle,
-.hamburger.is-open .hamb-bottom {
-  background-color: #1a1a1a;
-}
-.hamburger.is-open .hamb-top,
-.hamburger.is-open .hamb-bottom {
-  top: 50%;
-  margin-top: -2px;  
-}
-.hamburger.is-open .hamb-top { 
-  -webkit-transform: rotate(45deg);
-  -webkit-transition: -webkit-transform .2s cubic-bezier(.73,1,.28,.08);
-}
-.hamburger.is-open .hamb-middle { display: none; }
-.hamburger.is-open .hamb-bottom {
-  -webkit-transform: rotate(-45deg);
-  -webkit-transition: -webkit-transform .2s cubic-bezier(.73,1,.28,.08);
-}
-.hamburger.is-open:before {
-  content: '';
-  display: block;
-  width: 100px;
-  font-size: 14px;
-  color: #fff;
-  line-height: 32px;
-  text-align: center;
-  opacity: 0;
-  -webkit-transform: translate3d(0,0,0);
-  -webkit-transition: all .35s ease-in-out;
-}
-.hamburger.is-open:hover:before {
-  opacity: 1;
-  display: block;
-  -webkit-transform: translate3d(-100px,0,0);
-  -webkit-transition: all .35s ease-in-out;
+/* ---------------------------------------------------
+    SIDEBAR STYLE
+----------------------------------------------------- */
+
+.dashboard {
+    display: flex;
+    width: 100%;
+    align-items: stretch;
 }
 
-/*-------------------------------*/
-/*            Overlay            */
-/*-------------------------------*/
+#sidebar {
+    min-width: 250px;
+    max-width: 250px;
+    background: #141b35;
+    color: #fff;
+    transition: all 0.3s;
+}
 
+#sidebar.active {
+    margin-left: -250px;
+}
 
+#sidebar ul.components {
+    padding: 20px 0;
+    border-bottom: 1px solid #47748b;
+}
+
+#sidebar ul p {
+    color: #fff;
+    padding: 10px;
+}
+
+#sidebar ul li a {
+    padding: 10px;
+    font-size: 1.1em;
+    display: block;
+    text-decoration: none;
+    color: #fff;
+}
+
+#sidebar ul li a:hover {
+    color: #7386D5;
+    background: #fff;
+}
+
+ul ul a {
+    font-size: 0.9em !important;
+    padding-left: 30px !important;
+    background: #6d7fcc;
+}
+
+ul.CTAs {
+    padding: 20px;
+}
+
+ul.CTAs a {
+    text-align: center;
+    font-size: 0.9em !important;
+    display: block;
+    border-radius: 5px;
+    margin-bottom: 5px;
+}
+
+a.download {
+    background: #fff;
+    color: #7386D5 !important;
+}
+
+/* ---------------------------------------------------
+    CONTENT STYLE
+----------------------------------------------------- */
+
+#content {
+    width: 100%;
+    padding: 20px;
+    min-height: 100vh;
+    transition: all 0.3s;
+}
+
+/* ---------------------------------------------------
+    MEDIAQUERIES
+----------------------------------------------------- */
+
+@media (max-width: 768px) {
+    #sidebar {
+        margin-left: -250px;
+    }
+    #sidebar.active {
+        margin-left: 0;
+    }
+    #sidebarCollapse span {
+        display: none;
+    }
+}
 </style>
