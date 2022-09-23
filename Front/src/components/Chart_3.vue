7aea50064d3286735adbf4783bf515a4da2a904a
CKankou04 <>
2022-09-23 12:56:04 +0200

    Responsive third chart

--- Front/src/components/Chart_3.vue
+++ Front/src/components/Chart_3.vue
@@ -1,43 +1,32 @@
 <template>
-    <div id="chart">
-       <apexchart type="pie" width="380" :options="chartOptions" :series="series"></apexchart>
-     </div>
- </template>
+  <div id="chart">
+        <apexchart width="400" type="pie" :options="chartOptions" :series="series"></apexchart>
+      </div>
+</template>
  
  <script>
  
  export default {
-   name: 'Chart',
    data: function() {
      return {
         series: [25, 15, 44, 55, 17],
           chartOptions: {
             chart: {
-              width: '430',
               type: 'pie',
             },
             labels: ['TV en direct ', ' films', ' travailler', ' solo', 'équipe '],
-            theme: {
-              monochrome: {
-                enabled: true
-              }
-            },
-            plotOptions: {
-              pie: {
-                dataLabels: {
-                  offset: -5
+            responsive: [{
+              breakpoint: 200,
+              options: {
+                chart: {
+                  width:70
+                },
+                legend: {
+                  position: 'bottom',
+              
                 }
               }
-            },
-            dataLabels: {
-              formatter(val, opts) {
-                const name = opts.w.globals.labels[opts.seriesIndex]
-                return [name, val.toFixed(1) + '%']
-              }
-            },
-            legend: {
-              show: false
-            }
+            }]
           },
           
      }
