89b7c18a2deeaee37fbbe602efce2111f11549bc
Adama <adamagassama40.ag@gmail.com>
2022-10-04 23:58:48 +0200

    Fix: display errors messages controller

--- Back/app/Http/Controllers/Api/AnswerController.php
+++ Back/app/Http/Controllers/Api/AnswerController.php
@@ -6,6 +6,7 @@ use App\Http\Controllers\Controller;
 use App\Models\Answer;
 use App\Models\Visitor;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
 use Illuminate\Support\Str;
 
 class AnswerController extends Controller
@@ -24,7 +25,7 @@ class AnswerController extends Controller
             'data' => $answer
         ], 200);
     }
-
+    
     /**
      * Store a newly created resource in storage.
      *
@@ -33,29 +34,50 @@ class AnswerController extends Controller
      */
     public function store(Request $request)
     {
+        $validator = Validator::make($request->all(), [
+            'answersArray.*' => 'required',
+            'answersArray.1' => 'email',
+            'answersArray.2' => 'numeric',
+        ],[
+            'answersArray.*.required' => 'Veuillez renseigner tous les champs',
+            'answersArray.1.email' => 'Veuillez rentrer une adresse mail valide',
+            'answersArray.2.numeric' => 'Veuillez rentrer un entier pour l\'age',
+        ]);
 
-        foreach ($request->formData as $one) {
-            $one = json_decode($one);
-
-            if ($one->question_id == 1) {
-                $user = new Visitor();
-                $user->email = $one->answer;
-                $user->url = Str::random(20);
-                $user->save();
-            }
-            $answer = new Answer();
-            $answer->answer = $one->answer;
-            $answer->question_id = $one->question_id;
-            $answer->visitor_id = $user->id;
-            $answer->save();
+        if ($validator->fails()) {
+            return response()->json([
+                'msg' => $validator->errors()->first()
+            ], 422);
         }
+        else {
+            foreach ($request->answersArray as $key => $value ) {
+                if ($key == 1) {
+                    $email = $value;
+                    $visitor = Visitor::where('email', $email)->first();
+                    if (!empty($visitor)) {
+                        return response()->json(['msg' => 'Vous avez déjà participé à notre enqûete avec cette email'],422);
 
-        return response()->json(['text' => 'Toute l’équipe de Bigscreen vous remercie pour votre engagement.<br/>Grâce à
-                                            votre investissement, nous vous préparons une application toujours plus
-                                            facile à utiliser, seul ou en famille.<br>
-                                            Si vous désirez consulter vos réponse ultérieurement, vous pouvez consultez
-                                            cette adresse:',
-                                 'url'  =>  $user->url]);
+                    }else{
+                        $user = new Visitor();
+                        $user->email = $value;
+                        $user->url = Str::random(20);
+                        $user->save();
+                    }
+                }
+                $answer = new Answer();
+                $answer->answer = $value;
+                $answer->question_id = $key;
+                $answer->visitor_id = $user->id;
+                $answer->save();
+            }
+
+            return response()->json(['text' => 'Toute l’équipe de Bigscreen vous remercie pour votre engagement.<br/>Grâce à
+                                                        votre investissement, nous vous préparons une application toujours plus
+                                                        facile à utiliser, seul ou en famille.<br>
+                                                        Si vous désirez consulter vos réponse ultérieurement, vous pouvez consultez
+                                                        cette adresse:',
+                                    'url'  =>  $user->url]);
+        }
 
     }
 
