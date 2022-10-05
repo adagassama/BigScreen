dcb6098304c0524f80b51acab6a539c663b009b8
Adama <adamagassama40.ag@gmail.com>
2022-10-05 02:02:01 +0200

    Display all answers for all visitors

--- Front/src/components/Reponse.vue
+++ Front/src/components/Reponse.vue
@@ -1,26 +1,25 @@
 <template>
   <div class="container mr-0 ml-0 pl-0 pr-0">
     <h2 class="text-center pt-0 pb-4 mt-0 mb-4">Liste de Questions et leurs Réponses</h2>
-    <div class="row">
-      <div class="col-lg-12">
-        <div class="card cardF rounded shadow border-1">
+    <div class="row" v-for="visitor in visitors" :key="visitor.id">
+      <div class="col-lg-12" >
+        <div class="card cardF rounded shadow border-1" >
           <div class="card-body cbody rounded">
-            <div class="table-responsive">
+            <div class="table-responsive" >
               <table id="example" style="width:100%" class="table table-striped table-bordered" >
                 <thead>
                   <tr>
-                    <th class="col-3" style="color:darkblue">Numero Question</th>
+                    <th class="col-3">Numero Question</th>
                     <th class="col-5">Corps de la question</th>
                     <th class="col-8">Réponse de Question</th>
                   </tr>
                 </thead>
-                <tbody>
-                  <tr v-for="answer in answers" :key="answer.id">
-                    <td class="col-3">{{answer.question.title}}</td>
-                    <td class="col-5">{{answer.question.content}}</td>
-                    <td class="col-8 text-center">{{answer.answer}}</td>
+                <tbody v-for="answer in answers" :key="answer.id">
+                  <tr v-if="visitor.id == answer.visitor_id" style="list-style:none">
+                      <td class="col-3 text-center">{{answer.question_id}}</td>
+                      <td class="col-5">{{answer.question.content}}</td>
+                      <td class="col-8 text-center">{{answer.answer}}</td>
                   </tr>
-                  <br />
                 </tbody>
               </table>
             </div>
@@ -44,6 +43,14 @@ export default {
       .catch((error) => {
         console.log(error);
       });
+    axios
+    .get(`http://127.0.0.1:8000/api/visitors`)
+      .then((res) => {
+        this.visitors = res.data.data;
+      })
+      .catch((error) => {
+        console.log(error);
+      });
   },
   data() {
     return {
@@ -56,6 +63,9 @@ export default {
   </script>
   
   <style>
+  .row{
+    margin-bottom: 3em;
+  }
   .cardFirst{
       margin-left: 0;
       margin-right: 0;
